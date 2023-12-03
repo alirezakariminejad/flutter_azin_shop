8e5e35fea231614d4f4ebb4f33c3516eb704d56c
Alireza <alireza.kariminejad@yahoo.com>
2023-12-03 08:06:32 +0330

    Create Azin Shop App

---
+++ lib/constants/Constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final Color azinBlue = Color(0xff3B5EDF);
+final Color azinWhite = Color(0xffEEEEEE);
+final Color azinGray = Color(0xff858585);
+final Color azinGreen = Color(0xff1DB68B);
+final Color azinRed = Color(0xffD02026);
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_azin_shop_app/screens/First_Screen.dart';
+
+void main() {
+  runApp(const FirstScreen());
+}
