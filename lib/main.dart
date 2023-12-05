054618ff88f98106009cb942e48298d95812150e
Alireza <alireza.kariminejad@yahoo.com>
2023-12-05 07:26:19 +0330

    change to show ProductScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_azin_shop_app/screens/First_Screen.dart';
+import 'package:flutter_azin_shop_app/screens/product_screen.dart';
 
 void main() {
-  runApp(const FirstScreen());
+  runApp(const ProductScreen());
 }
