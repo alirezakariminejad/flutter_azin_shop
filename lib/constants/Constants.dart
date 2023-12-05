00bf899e7d36ea180266aac204da9a42f1eee65f
Alireza <alireza.kariminejad@yahoo.com>
2023-12-05 07:24:56 +0330

    change final to const

--- lib/constants/Constants.dart
+++ lib/constants/Constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-final Color azinBlue = Color(0xff3B5EDF);
-final Color azinWhite = Color(0xffEEEEEE);
-final Color azinGray = Color(0xff858585);
-final Color azinGreen = Color(0xff1DB68B);
-final Color azinRed = Color(0xffD02026);
+const Color azinBlue = Color(0xff3B5EDF);
+const Color azinWhite = Color(0xffEEEEEE);
+const Color azinGray = Color(0xff858585);
+const Color azinGreen = Color(0xff1DB68B);
+const Color azinRed = Color(0xffD02026);
