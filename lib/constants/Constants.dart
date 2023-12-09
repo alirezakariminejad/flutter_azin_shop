fb149b4b90155c7f51a1ef228ee8f8e50c6f4b35
Alireza <alireza.kariminejad@yahoo.com>
2023-12-09 07:49:49 +0330

    add new color

--- lib/constants/Constants.dart
+++ lib/constants/Constants.dart
@@ -5,3 +5,4 @@ const Color azinWhite = Color(0xffEEEEEE);
 const Color azinGray = Color(0xff858585);
 const Color azinGreen = Color(0xff1DB68B);
 const Color azinRed = Color(0xffD02026);
+const Color scaffoldGray = Color(0xffEEEEEE);
